93365109699ddb27f0dbdb2e338a4ab6d91f77b6
Dino Fusco <fooskow@MacBook-Pro-di-Dino.local>
2025-05-27 12:03:14 +0200

    Messaggio nuovo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello Antonio!</h1>')
+  res.send('<h1>Hello Antonio di nuovo!</h1>')
 })
 
 const PORT = 8080
